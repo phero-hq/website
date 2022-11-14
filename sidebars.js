03ad5553589610d9172ea44cadb1d09d6b2cf020
Jasper Haggenburg <jasper@pressplay.dev>
2022-11-14 18:04:44 +0100

    Fix capitalization of docs/introduction.mdx

--- sidebars.js
+++ sidebars.js
@@ -15,7 +15,7 @@
 const sidebars = {
   // By default, Docusaurus generates a sidebar from the docs folder structure
   docSidebar: [
-    "Introduction",
+    "introduction",
     "getting-started",
     {
       type: "category",
